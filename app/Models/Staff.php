e88aacd3301e8201b529449c06962708d014bad3
AphiwatNoiluea <eff675408@gmail.com>
2022-05-08 00:00:00 +0700

    Commit a little bit

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -9,4 +9,18 @@ class Staff extends Model
 {
     use HasFactory;
     protected $table = "staff";
+    protected $fillable = [
+        'id_staff',
+        'id_rfid',
+        'prefix',
+        'name_first',
+        'name_last',
+        'site',
+        'id_role',
+        'id_shif',
+        'staff_img',
+    ];
+    public $timestamps = false;
+
+
 }
