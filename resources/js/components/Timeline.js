e2f75148a922c687eb748daa8cee36e7825ad0ab
AphiwatNoiluea <eff675408@gmail.com>
2022-05-30 16:06:57 +0700

    30-05-2565

--- resources/js/components/Timeline.js
+++ resources/js/components/Timeline.js
@@ -4,15 +4,70 @@ import Chart from 'react-apexcharts'
 import ReactApexChart from 'react-apexcharts';
 import ShowTimeline from './Timeline/ShowTimeline';
 import ManageTimeline from './Timeline/ManageTimeline';
+
+
+
 class Timeline extends Component {
 
     constructor(props) {
             super(props);
-  
+
             this.state = {
                 timelineActivity : [],
                 timelineActivityDowntime : [],
                 timelineActivityRework : [],
+                showPrint : timelineOpject,
+                series: [
+                    {
+                      name: 'Blue',
+                      data: [
+                        {
+                          x: 'id-machine 1',
+                          y: [
+                            new Date("2022-05-18 06:44:46").getTime(),
+                            new Date("2022-06-18 06:44:46").getTime()
+                          ]
+                        },
+                      ]
+                    },
+
+                    ],
+                options: {
+                        chart: {
+                          height: 350,
+                          type: 'rangeBar'
+                        },
+                        plotOptions: {
+                          bar: {
+                            horizontal: true,
+                            barHeight: '50%',
+                            rangeBarGroupRows: true
+                          }
+                        },
+                        colors: [
+                          "#008FFB", "#00E36E", "#F0E040", "#DA4747"
+                        ],
+                        fill: {
+                          type: 'solid'
+                        },
+                        xaxis: {
+                          type: 'datetime'
+                        },
+                        legend: {
+                          position: 'right'
+                        },
+                        tooltip: {
+                          custom: function(opts) {
+                            const fromYear = new Date(opts.y1).getFullYear()
+                            const toYear = new Date(opts.y2).getFullYear()
+                            const values = opts.ctx.rangeBar.getTooltipValues(opts)
+                        
+                            return (
+                              ''
+                            )
+                          }
+                        }
+                      },
             }
         }
 
@@ -52,14 +107,23 @@ class Timeline extends Component {
 
     render() {
         return (
+            <>
             <div id="chart_timeline">
-                <ShowTimeline />
+                {/* <ShowTimeline /> */}
                 {/* {/* <div>{this.state.timelineActivity[0].id_activity}</div> */}
-                    {this.state.timelineActivityDowntime.map(function (x, i) {
-                        return <ManageTimeline key={i} data={x} /> 
+                
+                    {this.state.timelineActivityRework.map(function (x, i) {
+                        
                     })}
                 {/* <div>{this.state.timelineActivityRework[0].id_activity}</div> */}
+
+                <ReactApexChart options={this.state.options} series={this.state.series} type="rangeBar" height={350} />
+                    
+                
+
             </div>
+            
+            </>
         );
     }
 }
