7331f4169dc1f2d5b308e60bf161358cac8126a6
AphiwatNoiluea <eff675408@gmail.com>
2022-06-22 16:42:43 +0700

    downtime 22-06-2565

--- app/Models/ActivityDowntime.php
+++ app/Models/ActivityDowntime.php
@@ -8,5 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ActivityDowntime extends Model
 {
     use HasFactory;
-    protected $table = "activity_downtime";
+    protected $table = 'activity_downtime';
+    protected $fillable = [
+        'id_activity_downtime',
+        'id_task',
+        'id_machine',
+        'id_staff',
+        'shif',
+        'id_code_downtime',
+        'status_downtime',
+        'date_eff',
+        'time_start',
+        'time_close',
+        'total_work'
+    ];
+    public $timestamps = false;
 }
